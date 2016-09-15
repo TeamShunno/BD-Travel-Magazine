3288024921f83723203c24d924af75d97d7d6b8d
imranctg16 <imranhossain16.ctg@gmail.com>
2016-09-16 02:59:43 +0600

    Previous version had some bug

--- app/src/main/java/team/shunno/bdtm/MainActivity.java
+++ app/src/main/java/team/shunno/bdtm/MainActivity.java
@@ -53,16 +53,20 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        /// Drawer code
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+
+       ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        /// drawer code end
 
         /**
          * Init Database
